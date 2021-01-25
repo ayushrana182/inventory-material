cdcb2bdb5f77d991400d66a112c7fbada8407eb5
ayushrana182 <ayushrana182@gmail.com>
2021-01-25 13:43:57 +0545

    Added Tab Panel

--- src/views/reports/DashboardView/index.js
+++ src/views/reports/DashboardView/index.js
@@ -9,6 +9,7 @@ import TasksProgress from './TasksProgress';
 import TotalCustomers from './TotalCustomers';
 import TotalProfit from './TotalProfit';
 import TrafficByDevice from './TrafficByDevice';
+import ItemTabs from './Tabs';
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -36,11 +37,11 @@ const Dashboard = () => {
             <TasksProgress />
           </Grid>
           {/* <Grid item lg={3} sm={6} xl={3} xs={12}>
-            <TotalProfit />
+            
           </Grid> */}
-          {/* <Grid item lg={12} md={12} xl={12} xs={12}>
-            <LatestOrders />
-          </Grid> */}
+          <Grid item lg={12} md={12} xl={12} xs={12}>
+            <ItemTabs />
+          </Grid>
 
           {/* <Grid item lg={12} md={12} xl={9} xs={12}>
             <Sales />
